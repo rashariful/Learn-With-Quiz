7a70ef7b98ac09cca3be7fe533b344e8fbbdbcb2
md shariful islam <raislam.shariful@gmail.com>
2022-10-11 17:30:47 +0600

    hero secton updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,9 +13,9 @@ const Home = () => {
                     <div className="flex flex-col lg:flex-row justify-between gap-6 sm:gap-10 md:gap-16">
                        
                         <div className="xl:w-5/12 flex flex-col justify-center items-center lg:items-start sm:text-center lg:text-left lg:py-12 xl:py-24">
-                            <h1 className="text-black-800 text-4xl sm:text-5xl md:text-6xl font-bold mb-8 md:mb-12">Revolutionary way to build the web</h1>
+                            <h1 className="text-black-800 text-4xl sm:text-5xl md:text-6xl font-bold mb-8 md:mb-12">05 Most Asked Interview Questions</h1>
 
-                            <p className="lg:w-4/5 text-gray-500 xl:text-lg leading-relaxed mb-8 md:mb-12">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random.</p>
+                            <p className="lg:w-4/5 text-gray-500 xl:text-lg leading-relaxed mb-8 md:mb-12">If you are a complete beginner then in this article, we will dive into the top 100 important Web development Interview Questions and Answers.</p>
 
                             <form className="w-full md:max-w-md flex gap-2">
                                 <input placeholder="Email" className="w-full flex-1 bg-gray-50 text-gray-800 placeholder-gray-500 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
@@ -25,7 +25,7 @@ const Home = () => {
                         </div>
                        
                         <div className="xl:w-5/12 h-48 lg:h-auto bg-gray-100 overflow-hidden shadow-lg rounded-lg">
-                            <img src="https://images.unsplash.com/photo-1620206343767-7da98185edd4?auto=format&q=75&fit=crop&w=1000" loading="lazy" alt="Photo by Fakurian Design" className="w-full h-full object-cover object-center" />
+                            <img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200611200432/Top-10-System-Design-Interview-Questions-and-Answers.png" loading="lazy" alt="Photo by Programming hero student designed" className="w-full h-full object-cover object-center" />
                         </div>
                        
                     </div>
